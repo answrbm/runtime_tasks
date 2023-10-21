7620286a72a73c271caccdc88680411f4557c06b
Ansar Bektassov <bektasovansar@gmail.com>
2023-10-21 14:51:58 +0600

    Updated Book class

--- src/classwork/projects/library/Book.java
+++ src/classwork/projects/library/Book.java
@@ -1,15 +1,18 @@
 package classwork.projects.library;
 
+import java.util.ArrayList;
+
 public class Book {
 
-    private int id;
+    private int bookId;
     private String title;
     private String author;
-    private double rating;
-    private int ratingQuantity; // quantity of ratings for book
+    private double averageRating;
+    private boolean isAvailable;
+    private ArrayList<Double> ratings; // ratings for book
 
-    public Book(int id, String title, String author) {
-        this.id = id;
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
         this.title = title;
         this.author = author;
     }
@@ -18,8 +21,28 @@ public class Book {
         return title;
     }
 
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public void calculateRating() {
+        double sum = 0;
+        for(double rating : ratings) {
+            sum += rating;
+        }
+        System.out.println("Average rating for book: " + sum/ratings.size());
+    }
+
+    public void addRating(double rating) {
+        ratings.add(rating);
+    }
+
     // Method to display book details
     public void display() {
-        System.out.println("Book: " + title + " | book id: " + id + " | author: " + author);
+        System.out.println("Book: " + title + " | book id: " + bookId + " | author: " + author + " | rating: " + averageRating);
     }
 }
