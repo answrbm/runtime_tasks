25116c5360d6ce9a62dd16e730796ec1d94c0451
Ansar Bektassov <bektasovansar@gmail.com>
2023-10-21 14:38:28 +0600

    Created Teacher and Subject classes

---
+++ src/classwork/projects/library/Subject.java
@@ -0,0 +1,27 @@
+package classwork.projects.library;
+
+public class Subject {
+
+    private int subjectId;
+    private String subjectName;
+    private Teacher teacher;
+
+    public Subject(int subjectId, String subjectName, Teacher teacher) {
+        this.subjectId = subjectId;
+        this.subjectName = subjectName;
+        this.teacher = teacher;
+    }
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    // Method to display subject details
+    public void display() {
+        System.out.println("Subject: " + subjectName + " | subject id: " + subjectId + " | teacher: " + teacher.getName());
+    }
+}
