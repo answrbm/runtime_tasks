4ca0e3c9eaebb5e72eb331a34d4c67886bd2b8b9
Ansar Bektassov <bektasovansar@gmail.com>
2023-11-17 17:46:33 +0600

    Finished Pool task

---
+++ src/lesson7/pooltask/Rowboat.java
@@ -0,0 +1,7 @@
+package lesson7.pooltask;
+
+public class Rowboat extends Boat {
+    public void rowTheBoat() {
+        System.out.print("Давай, Наташа!");
+    }
+}
---
+++ src/lesson7/pooltask/Sailboat.java
@@ -0,0 +1,7 @@
+package lesson7.pooltask;
+
+public class Sailboat extends Boat {
+    public void move() {
+        System.out.println("Поднять паруса!");
+    }
+}
