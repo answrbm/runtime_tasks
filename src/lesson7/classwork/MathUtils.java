06a85fbade60bdb1add3d9c3a7e5721c5d5a0cd9
Ansar Bektassov <bektasovansar@gmail.com>
2023-11-25 14:42:26 +0600

    Updated MathUtils

--- src/lesson7/classwork/MathUtils.java
+++ src/lesson7/classwork/MathUtils.java
@@ -2,19 +2,25 @@ package lesson7.classwork;
 
 public class MathUtils {
 
+    private static int counter = 0;
+
     public Integer sum(Integer x, Integer y) {
+        counter++;
         return x + y;
     }
 
     public Integer sub(Integer x, Integer y) {
+        counter++;
         return x - y;
     }
 
     public Integer mult(Integer x, Integer y) {
+        counter++;
         return x * y;
     }
 
     public Integer div(Integer x, Integer y) {
+        counter++;
         return x / y;
     }
 }
