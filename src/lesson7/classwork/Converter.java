b78b6a89b03d61098b1f27dc1807f7ac6fa7c0de
Ansar Bektassov <bektasovansar@gmail.com>
2023-11-25 14:46:17 +0600

    Updated Converter

--- src/lesson7/classwork/Converter.java
+++ src/lesson7/classwork/Converter.java
@@ -3,26 +3,26 @@ package lesson7.classwork;
 public class Converter {
 
     public static Integer convert(Double d) {
-        return Integer.parseInt(String.valueOf(d));
+        return d.intValue();
     }
 
     public static Integer convert(Float d) {
-        return Integer.parseInt(String.valueOf(d));
+        return d.intValue();
     }
 
     public static Integer convert(Long d) {
-        return Integer.parseInt(String.valueOf(d));
+        return d.intValue();
     }
 
     public static Float convert(Integer i) {
-        return Float.parseFloat(String.valueOf(i));
+        return i.floatValue();
     }
 
     public static Double convert(Byte i) {
-        return Double.parseDouble(String.valueOf(i));
+        return i.doubleValue();
     }
 
     public static Long convert(Short i) {
-        return Long.parseLong(String.valueOf(i));
+        return i.longValue();
     }
 }
