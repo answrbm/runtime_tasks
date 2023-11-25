e14ebe9efabe3ae1ee0f22a7bb0775e5068b1464
Ansar Bektassov <bektasovansar@gmail.com>
2023-11-25 14:48:09 +0600

    Updated Constants

--- src/lesson7/classwork/Constants.java
+++ src/lesson7/classwork/Constants.java
@@ -2,7 +2,7 @@ package lesson7.classwork;
 
 public class Constants {
 
-    public static final double PI = Math.PI;
-    public static final double EULER_NUMBER = Math.E;
+    public static final double PI = 3.141592653589793238462643383279502884197;
+    public static final double EULER_NUMBER = 2.7182818284590452353602874713527;
     public static final double GRAVITY_CONSTANT = 6.673e-11;
 }
