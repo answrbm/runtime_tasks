29d52694376268db2459a6049aee94f3c0110597
Ansar Bektassov <ansar_bektassov@epam.com>
2024-03-27 14:42:07 +0500

    Updated Main in task tracker

--- src/classwork/projects/tasktracker/exceptions/Main.java
+++ src/classwork/projects/tasktracker/exceptions/Main.java
@@ -7,35 +7,5 @@ public class Main {
 
     public static void main(String[] args) throws TaskNotFoundException {
         ManagerImpl managerImpl = new ManagerImpl();
-
-        Task task1 = new Task(1,"Task1 summary","Task1 description");
-        Task task2 = new Task(2,"Task2 summary","Task2 description");
-
-        Epic epic1 = new Epic(3,"Epic1 summary","Epic1 description");
-        epic1.addSubtasks(new Subtask(5,"Subtask1 summary","Subtask1 description",epic1));
-        epic1.addSubtasks(new Subtask(6,"Subtask2 summary","Subtask2 description",epic1));
-
-        Epic epic2 = new Epic(4,"Epic2 summary","Epic2 description");
-        epic2.addSubtasks(new Subtask(7,"Subtask3 summary","Subtask3 description",epic2));
-
-        managerImpl.addTask(task1);
-        managerImpl.addTask(task2);
-        managerImpl.addTask(epic1);
-        managerImpl.addTask(epic2);
-
-        managerImpl.getTasks().forEach(System.out::println);
-        System.out.println("********");
-
-        epic1.getSubtasks().get(0).setStatus(Status.IN_PROGRESS);
-        epic2.getSubtasks().get(0).setStatus(Status.DONE);
-        managerImpl.updateTask(epic1);
-        managerImpl.updateTask(epic2);
-
-        managerImpl.getTasks().forEach(System.out::println);
-        System.out.println("********");
-
-        managerImpl.deleteById(task1.getTaskId());
-
-        managerImpl.getTasks().forEach(System.out::println);
     }
 }
